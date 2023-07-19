942e16a9a008ba3db57d185f27639979946c5d7a
thephoenixpixel <jan6nik2007@gmail.com>
2023-07-19 20:55:28 +0200

    Keine Ahnung

--- src/data/Task.rs
+++ src/data/Task.rs
@@ -1,15 +1,28 @@
 use std::fs;
 use std::env;
+use std::io::Write;
+use std::path::PathBuf;
+use std::process::Child;
+use std::thread;
+use std::time::Duration;
 use std::process::Command;
-use std::path::{Path, PathBuf};
+
+use reqwest::blocking::Client;
 use serde_yaml;
 use fs_extra::dir::{copy, CopyOptions};
+use serde_yaml::Value;
+
 pub struct Task {
     // Task Struktur
     pub name: String,
     pub minservicecount: u32,
     pub maxram: u32,
-    pub template: String
+    pub template: String,
+}
+
+pub struct ServerProcess {
+    pub name: String,
+    pub process: Option<Child>,
 }
 
 impl Task {
@@ -28,13 +41,13 @@ impl Task {
     pub fn get_task(name: &str) -> Option<Task> {
         let exe_path = env::current_exe()
             .expect("Ausführungs path konnte nicht gefunden werden");
-        
+
         let mut task_path = exe_path.clone();
         task_path.pop();
         task_path.push("task");
-        
-        
-        
+
+
+
         // YAML-Dateien lesen
         let yaml_files = fs::read_dir(task_path)
             .expect("Fehler beim Lesen des Task-Ordners")
@@ -50,15 +63,15 @@ impl Task {
 
         // Nach dem Task mit dem angegebenen Namen suchen
         for file_path in yaml_files {
-            
+
             //read content
             let file_content = fs::read_to_string(&file_path)
                 .expect("for schliefe yml task");
-            
+
             let config: serde_yaml::Value = serde_yaml::from_str(&file_content)
                 .expect("Error beim Deserialisieren der config datei");
             //
-            
+
             let _task_name = config["name"].as_str();
 
             if let (Some(task_name), Some(minservicecount), Some(maxram), Some(template)) = (
@@ -92,66 +105,235 @@ impl Task {
         self.maxram
     }
 
+    //set ram
+    pub fn set_maxram(&self){
+        let exe_path = env::current_exe()
+            .expect("Ausführungs path konnte nicht gefunden werden");
+
+        let mut task_path = exe_path.clone();
+        task_path.pop();
+        task_path.push("task");
+
+        // YAML-Dateien lesen
+        let yaml_files = fs::read_dir(task_path)
+            .expect("Fehler beim Lesen des Task-Ordners")
+            .filter_map(|entry| {
+                let entry = entry.expect("Fehler beim Lesen des Verzeichniseintrags");
+                let file_path = entry.path();
+                if file_path.is_file() {
+                    Some(file_path)
+                } else {
+                    None
+                }
+            });
+
+        // Nach dem Task mit dem angegebenen Namen suchen
+        for file_path in yaml_files {
+
+            //read content
+            let file_content = fs::read_to_string(&file_path)
+                .expect("for schliefe yml task");
+
+            let config: serde_yaml::Value = serde_yaml::from_str(&file_content)
+                .expect("Error beim Deserialisieren der task datei");
+
+            config["maxram"] = serde_yaml::Value::Number(serde_yaml::Number::from(self.maxram));
+        }
+    }
+
     //get template
     pub fn get_template(&self) -> &str{
         &self.template
     }
 
+    //set template
+    pub fn set_template(&self){
+        let exe_path = env::current_exe()
+            .expect("Ausführungs path konnte nicht gefunden werden");
 
-    pub fn start_as_service(&self) {
+        let mut task_path = exe_path.clone();
+        task_path.pop();
+        task_path.push("task");
+
+        // YAML-Dateien lesen
+        let yaml_files = fs::read_dir(task_path)
+            .expect("Fehler beim Lesen des Task-Ordners")
+            .filter_map(|entry| {
+                let entry = entry.expect("Fehler beim Lesen des Verzeichniseintrags");
+                let file_path = entry.path();
+                if file_path.is_file() {
+                    Some(file_path)
+                } else {
+                    None
+                }
+            });
+
+        // Nach dem Task mit dem angegebenen Namen suchen
+        for file_path in yaml_files {
+
+            //read content
+            let file_content = fs::read_to_string(&file_path)
+                .expect("for schliefe yml task");
+
+            let config: serde_yaml::Value = serde_yaml::from_str(&file_content)
+                .expect("Error beim Deserialisieren der task datei");
+
+            config["template"] = serde_yaml::Value::String(self.template.clone());
+        }
+    }
+
+    pub fn create(&self) -> bool{
+        let mut exe_path:PathBuf = env::current_exe().expect("Fehler beim Abrufen des Ausführungspfads");
+        exe_path.pop();
+        let mut task_path = exe_path.clone();
+        task_path.push("task");
+        task_path.push(format!("{}.yml", &self.name));
+        task_path.to_str().expect("Error beim confertiren des task_path in einen String").to_string();
+
+        let mut config_task_path = exe_path.clone();
+        config_task_path.push("config");
+        config_task_path.push("task.yml");
+        config_task_path.to_str().expect("Error beim confertiren des config_task_path in einen String").to_string();
+
+        //lesen des inhalts der deault task datei
+        let default_file_content = fs::read_to_string(config_task_path);
+
+        // YAML-Wert aus dem Inhalt erstellen
+        let binding = default_file_content.expect("Error beim default_file_content_str");
+        let default_file_content_str: &str = &binding.as_str();
+
+        let mut new_content: Value = match serde_yaml::from_str(default_file_content_str) {
+            Ok(value) => value,
+            Err(error) => {
+                println!("Fehler beim Konvertieren des Inhalts in einen YAML-Wert: {}", error);
+                return false;
+            }
+        };
+
+        // Werte ersetzen
+        new_content["name"] = serde_yaml::Value::String(self.name.clone());
+        //new_content["minservicecount"] = serde_yaml::Value::Number(serde_yaml::Number::from(self.minservicecount));
+        //new_content["maxram"] = serde_yaml::Value::Number(serde_yaml::Number::from(self.maxram));
+        new_content["template"] = serde_yaml::Value::String(self.template.clone());
+
+        let mut file = match fs::File::create(task_path) {
+            Ok(file) => file,
+            Err(error) => {
+                println!("Fehler beim Erstellen der neuen Task-Datei: {}", error);
+                return false;
+            }
+        };
+
+        // Neue YAML-Datei erstellen
+        let yaml_string = match serde_yaml::to_string(&new_content) {
+            Ok(string) => string,
+            Err(error) => {
+                println!("Fehler beim Konvertieren des YAML-Werts in einen String: {}", error);
+                return false;
+            }
+        };
+
+        if let Err(error) = file.write_all(yaml_string.as_bytes()) {
+            println!("Fehler beim Schreiben in die Task-Datei: {}", error);
+            return false;
+        }
+        return true;
+    }
+    pub fn start_as_service(&mut self) {
         println!("start_as_serice");
-        println!();
-        // Implementiere hier deine Logik zum Starten des Servers als Service
+
         let exe_path = env::current_exe()
             .expect("Error beim lesen des exe path");
-        
+
         let mut template_path = exe_path.clone();
         template_path.pop();
         template_path.push("template");
         template_path.push(&self.template);
-        
+
         println!("{:?}", template_path);
-        
+
         let mut service_path = exe_path.clone();
         service_path.pop();
         service_path.push("service");
         service_path.push("temp");
-    
+
         println!("{:?}", service_path);
-    
+
+        //-----------------------
+
+        //gloabel hier??
+
+        //-----------------------
+
+
         if service_path.exists() && service_path.is_dir(){
             fs::remove_dir_all(&service_path).expect("Error beim löschen des temp ordners");
             fs::create_dir_all(&service_path).expect("Error beim erstellen des temp ordners");
         }
-    
+
         let mut options = CopyOptions::new();
         options.overwrite = true;
-    
-        match copy(template_path, service_path, &options) {
+
+        match copy(&template_path, &service_path, &options) {
             Ok(_) => println!("Ordner erfolgreich kopiert"),
             Err(e) => println!("Fehler beim Kopieren des Ordners: {}", e),
         };
-    
-        // Starte den MC-Server
-        
-        let service_path_proxy = service_path.join("Proxy");
-        let jar_path = service_path_proxy.join("velo.jar");
-        let jar_path_str = jar_path.to_string_lossy().to_string();
-    
-        let output = Command::new("java")
-            .arg("-Xmx1G")
-            .arg("-jar")
-            .arg(&jar_path_str)
-            .output()
+
+        thread::sleep(Duration::from_secs(5));
+
+        //--------------------------------------
+
+        //umbennen des server ordners
+        // bearbeitung der configs
+        //vllt noch hier den gloabl importieren
+
+        //---------------------------------------
+
+        service_path.push("server");
+
+        let mut service_path_jar = service_path.clone();
+        service_path_jar.push("paper.jar");
+
+        let service_path_string_jar = service_path_jar.to_str().expect("Fehler beim Konvertieren des Jar-Pfads").to_string();
+        let service_path_string = service_path.to_str().expect("Fehler beim Konvertieren des Pfads").to_string();
+
+        //start the server
+        let server = Command::new("java")
+            .args(&["-Xmx1G", "-jar", &service_path_string_jar])
+            .current_dir(&service_path_string)
+            .spawn()
             .expect("Fehler beim Starten des Servers");
-    
-        if output.status.success() {
-            println!("Server erfolgreich gestartet");
-        } else {
-            let error_message = String::from_utf8_lossy(&output.stderr);
-            println!("Fehler beim Starten des Servers: {}", error_message);
+
+
+        //webhook anfrage an den proxy
+        let webhook_url = "http://localhost:8000/webhook"; // Passe die URL entsprechend an
+
+        // Erstelle eine reqwest-Clientinstanz
+        let client = Client::new();
+
+        // Definiere die Daten, die du an das Java-Plugin senden möchtest
+        let data = [
+            ("name", &self.name),
+            ("host", &"127.0.0.1".to_string()),
+            ("port", &"25555".to_string()),
+        ];
+
+        // Führe die HTTP-POST-Anfrage durch
+        match client.post(webhook_url).form(&data).send() {
+            Ok(response) => {
+                if response.status().is_success() {
+                    println!("Webhook-Anfrage erfolgreich gesendet.");
+                    // Hier kannst du die Antwort des Java-Plugins verarbeiten, falls gewünscht.
+                } else {
+                    println!("Fehler beim Senden der Webhook-Anfrage: {:?}", response.status());
+                }
+            }
+            Err(e) => {
+                println!("Fehler beim Senden der Webhook-Anfrage: {:?}", e);
+            }
         }
     }
-    
-    
+
+
 }
+
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
 use std::fs;
+use std::io;
 use std::env;
-use crate::data::task::Task;
-
+use std::path::{PathBuf};
+use std::thread::available_parallelism;
+use crate::data::Service::Service;
+use crate::data::Task::Task;
 /*
 use std::f32::consts::E;
 use reqwest;
@@ -9,56 +12,201 @@ use reqwest::blocking::get;
 use bib::dir::Dir;
 use data::group;
 use data::service;
-mod bib{
-    pub mod dir;
-}
+
 */
 mod data{
-    pub mod task;
-    pub mod service;
+    pub mod Task;
+    pub mod Service;
     pub mod group;
 }
 
 
-fn main() -> Result<(), Box<dyn std::error::Error>>{
+fn main(){
 
     println!("Start Game Cloud");
 
     //read file and dirs
-    let exe_path = env::current_exe().expect("Fehler beim Abrufen des Ausführungspfads");
-    let mut task_path = exe_path.clone();
-    task_path.pop();
-    task_path.push("task");
+    let mut exe_path:PathBuf = env::current_exe().expect("Fehler beim Abrufen des Ausführungspfads");
+    exe_path.pop();
+    check_dir(exe_path);
 
-    if !task_path.exists() {
-        // Ordner erstellen, falls nicht vorhanden
-        fs::create_dir(&task_path).expect("Fehler beim Erstellen des Task-Ordners");
-    }
 
-    let task = Task::get_task("Proxy").expect("jou error bei get task");
-    
+
+    let mut task = Task::get_task("server").expect("jou error bei get task");
+
     println!("Name: {}", task.get_name());
     println!("Min Service Count: {}", task.get_minservicecount());
     println!("Max Ram: {}", task.get_maxram());
     println!("Template: {}", task.get_template());
 
 
-    task.start_as_service();
+    //task.start_as_service();
+
+    // cmd
+    loop{
+        let mut input = String::new();
+
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Error beim lesen der eingabe");
+
+
+        if !user_input(&input.trim().split_whitespace().collect::<Vec<&str>>()) {
+            break;
+        }
+    }
 
 
     //end
     println!("BB");
-    Ok(())
 }
 
+fn user_input(args: &[&str]) -> bool{
+    if let Some(command) = args.get(0) {
+        match command {
+            &"stop" => {
+                println!("stop");
 
 
+
+                return false;
+            }
+            &"task"=> {
+                println!("task");
+                if let Some(sub0) = args.get(1) {
+                    match sub0 {
+                        &"create" => {
+                            println!("task create");
+                            if let Some(sub1) = args.get(2) {
+                                println!("task create {}", sub1);
+                                if let Some(sub2) = args.get(3) {
+                                    match sub2 {
+                                        &"proxy" => {
+                                            println!("task create {} proxy", sub1);
+                                            println!("task erfolgreich erstellt (proxy)");
+
+                                            let task = Task::new(sub1, 0, 1028, sub1);
+
+                                            task.create();
+
+                                        }
+                                        &"server" => {
+                                            println!("task create {} server", sub1);
+                                            println!("task erfolgreich erstellt (Servertask )");
+
+                                            let task = Task::new(sub1, 0, 1028, sub1);
+
+                                            task.create();
+
+                                        }
+                                        _ => {
+                                            println!("dies ist kein gültiger typ")
+                                        }
+                                    }
+                                } else {
+                                    println!("Bitte gebe ein typ ein <server/proxy>");
+                                }
+                            } else {
+                                println!("Bitte gebe einen namen für die Task ein");
+                                println!("task create <name> <server/Proxy>");
+                            }
+                        }
+                        &"delete" => {
+                            println!("task delete");
+                            if let Some(sub1) = args.get(2) {
+
+                            } else{
+                                println!("Bitte gebe einen namen der Task ein den sie deleten wollen");
+                                println!("task delete <name>");
+                            }
+                        }
+                        _ => {
+                            println!("diesen sub command gibt es nicht");
+                            println!("Use << help >> um dir die Commands anzeigen zu lassen");
+                        }
+                    }
+                } else {
+                    println!("task create <name> <Server/Proxy>");
+                    println!("task delete <name>");
+                }
+            }
+
+            &"help" => {
+                println!("stop -> stoppen der cloud und damit allen services die gestartet sind");
+                println!("help -> anzeigen von den commands");
+                println!("task -> bearbeiten der tasks");
+                println!("service -> interagiren mit den services");
+            }
+
+            &"service" => {
+                println!("service");
+                if let Some(sub0) = args.get(1){
+                    match sub0 {
+                        &"start" => {
+                            if let Some(sub1) = args.get(2){
+                                let service = Service::new(sub1);
+                                service.start();
+                            }else {
+                                println!("bitte gebe ein task name ein");
+                                println!("damit dieser als service starten kann");
+                            }
+
+                        }
+                        _ => {
+                            println!("diesen sub command gibt es nicht");
+                            println!("Use << help >> um dir die Commands anzeigen zu lassen");
+                        }
+                    }
+                }else {
+                    println!("service start <task_name>");
+                }
+            }
+            _ => {
+                println!("Ich wusste garnicht das es diesen Command gibt");
+                println!("Benutze doch einfach einen von denen ->");
+                println!("Use << help >> um sie dir anzeigen zu lassen");
+            }
+        }
+    }else {
+        print!("kein Command");
+    }
+    return true;
+}
+
+fn shutdown(){
+
+}
+
+fn check_dir(exe_path: PathBuf){
+
+    {
+        //task dir
+        let mut task_path = exe_path.clone();
+        task_path.push("task");
+        if !task_path.exists() {
+            // Ordner erstellen, falls nicht vorhanden
+            fs::create_dir(&task_path).expect("Fehler beim Erstellen des Task-Ordners");
+        }
+    }
+    {
+        //teplate dir
+        let mut template_path = exe_path.clone();
+        template_path.push("template");
+        if !template_path.exists(){
+            fs::create_dir(&template_path)
+                .expect("Error beim erstellen des Template Ordners");
+        }
+
+    }
+
+
+}
 /*if !cloud_file_config_path.exists() {
         let url = "http://dev.phoenixcraft.eu/cloud/config.yml";
         let response = get(url)?.text()?;
-        
+
         let mut file = File::create(&cloud_file_config_path)?;
         file.write_all(response.as_bytes())?;
-        
+
         println!("Datei erstellt von {}", url);
     }*/
