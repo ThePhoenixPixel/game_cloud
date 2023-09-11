91d9b02d40e3d44e01d5d40af711b9085ecdba47
ThePhoenixPixel <code@thephoenixpixel.de>
2023-09-11 20:33:27 +0200

    add Cmd Command task info <name> (auser templates) print all

--- src/cmd/command/cmd_task.rs
+++ src/cmd/command/cmd_task.rs
@@ -1,6 +1,4 @@
-use std::sync::atomic::compiler_fence;
 use crate::config::Config;
-use crate::data::task;
 use crate::data::task::Task;
 
 pub struct CmdTask;
@@ -36,6 +34,24 @@ impl CmdTask{
         if let  Some(arg2) = args.get(2) {
             let task = Task::get_task(arg2.to_string()).unwrap();
 
+            //print task
+            println!("{} | Type             | Wert", Config::get_prefix());
+            println!("{} | ----------------------------------------------", Config::get_prefix());
+            println!("{} | Name             | {}", Config::get_prefix(), task.get_name());
+            println!("{} | Delete On Stop   | {}", Config::get_prefix(), task.get_delete_on_stop());
+            println!("{} | Static Service   | {}", Config::get_prefix(), task.get_static_service());
+            println!("{} | Nodes            | {:?}", Config::get_prefix(), task.get_nodes());
+            //print Software
+            println!("{} | Software:        | ", Config::get_prefix());
+            println!("{} |      Type        | {}", Config::get_prefix(), task.get_software().get_software_type());
+            println!("{} |      Name        | {}", Config::get_prefix(), task.get_software().get_name());
+            println!("{} |      Max Ram     | {}", Config::get_prefix(), task.get_software().get_max_ram());
+            //print Port
+            println!("{} | Start Port       | {}", Config::get_prefix(), task.get_start_port());
+            println!("{} | MinServiceCount  | {}", Config::get_prefix(), task.get_min_service_count());
+            println!("{} | Groups           | {:?}", Config::get_prefix(), task.get_groups());
+            //print Template
+            println!("{} | Templates:       | ", Config::get_prefix());
 
         }
     }
