00114ab81c8c5b0d4456cab62d6088331bb70e4c
BuildTools <unconfigured@null.spigotmc.org>
2023-09-13 07:51:34 +0200

    fix crash cmd task info <name> -> does not exsit

--- src/cmd/command/cmd_task.rs
+++ src/cmd/command/cmd_task.rs
@@ -35,26 +35,32 @@ impl CmdTask{
 
     fn info(args: &Vec<String>) {
         if let  Some(arg2) = args.get(1) {
-            let task = Task::get_task(arg2.to_string()).unwrap();
+            if let Some(task) = Task::get_task(arg2.to_string()){
+                //print task
+                println!("{} | Type             | Wert", Config::get_prefix());
+                println!("{} | ----------------------------------------------", Config::get_prefix());
+                println!("{} | Name             | {}", Config::get_prefix(), task.get_name());
+                println!("{} | Delete On Stop   | {}", Config::get_prefix(), task.get_delete_on_stop());
+                println!("{} | Static Service   | {}", Config::get_prefix(), task.get_static_service());
+                println!("{} | Nodes            | {:?}", Config::get_prefix(), task.get_nodes());
+                //print Software
+                println!("{} | Software:        | ", Config::get_prefix());
+                println!("{} |      Type        | {}", Config::get_prefix(), task.get_software().get_software_type());
+                println!("{} |      Name        | {}", Config::get_prefix(), task.get_software().get_name());
+                println!("{} |      Max Ram     | {}", Config::get_prefix(), task.get_software().get_max_ram());
+                //print Port
+                println!("{} | Start Port       | {}", Config::get_prefix(), task.get_start_port());
+                println!("{} | MinServiceCount  | {}", Config::get_prefix(), task.get_min_service_count());
+                println!("{} | Groups           | {:?}", Config::get_prefix(), task.get_groups());
+                //print Template
+                println!("{} | Templates:       | ", Config::get_prefix());
 
-            //print task
-            println!("{} | Type             | Wert", Config::get_prefix());
-            println!("{} | ----------------------------------------------", Config::get_prefix());
-            println!("{} | Name             | {}", Config::get_prefix(), task.get_name());
-            println!("{} | Delete On Stop   | {}", Config::get_prefix(), task.get_delete_on_stop());
-            println!("{} | Static Service   | {}", Config::get_prefix(), task.get_static_service());
-            println!("{} | Nodes            | {:?}", Config::get_prefix(), task.get_nodes());
-            //print Software
-            println!("{} | Software:        | ", Config::get_prefix());
-            println!("{} |      Type        | {}", Config::get_prefix(), task.get_software().get_software_type());
-            println!("{} |      Name        | {}", Config::get_prefix(), task.get_software().get_name());
-            println!("{} |      Max Ram     | {}", Config::get_prefix(), task.get_software().get_max_ram());
-            //print Port
-            println!("{} | Start Port       | {}", Config::get_prefix(), task.get_start_port());
-            println!("{} | MinServiceCount  | {}", Config::get_prefix(), task.get_min_service_count());
-            println!("{} | Groups           | {:?}", Config::get_prefix(), task.get_groups());
-            //print Template
-            println!("{} | Templates:       | ", Config::get_prefix());
+            } else {
+                //task not exsits
+                println!("{} Task does not exsists", Config::get_prefix());
+            }
+
+
 
         }
     }
