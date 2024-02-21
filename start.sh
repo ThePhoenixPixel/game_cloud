e1428ab1ed1477e07a02885dbf9edc9cae139061
ThePhoenixPixel <code@thephoenixpixel.de>
2024-02-21 17:40:45 +0100

    update start.sh

--- start.sh
+++ start.sh
@@ -1,21 +1,10 @@
-#!/bin/sh
-cd "$(dirname "$(readlink -fn "$0")")" || exit 1
-
-# check if java is installed
-if [ -x "$(command -v java)" ]; then
-  # if screen is present use that
-  # this check is elevated as tmux is sometimes present by default
-  if [ -x "$(command -v screen)" ]; then
-    # DO NOT CHANGE THE SUPPLIED MEMORY HERE. THIS HAS NO EFFECT ON THE NODE INSTANCE. USE THE launcher.cnl INSTEAD
-    screen -DRSq ./game_cloud
-  elif [ -x "$(command -v tmux)" ]; then
-    # DO NOT CHANGE THE SUPPLIED MEMORY HERE. THIS HAS NO EFFECT ON THE NODE INSTANCE. USE THE launcher.cnl INSTEAD
-    tmux new-session -As ./game_cloud
-  else
-    echo "No screen or tmux installation found, you need to install at least one of them to run CloudNet"
-    exit 1
-  fi
+#check screen install or tmux
+if [ -x "$(command -v screen)" ]; then
+  screen -DRSq game_cloud ./game_cloud
+elif [ -x "$(command -v tmux)" ]; then
+  tmux new-session -As ./game_cloud
 else
-  echo "No valid java installation was found, please install java in order to run CloudNet"
+  echo "No screen or tmux installation found, you need to install at least one of them to run GameCloud"
   exit 1
 fi
+
