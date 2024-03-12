bae7a7b98f41189cf8219844697cf649ba180d12
BuildTools <unconfigured@null.spigotmc.org>
2024-03-12 09:10:06 +0100

    update command me

--- src/terminal/command/cmd_me.rs
+++ src/terminal/command/cmd_me.rs
@@ -1,33 +1,19 @@
-use psutil::process::Process;
+use sysinfo::{Pid, System};
 
 use crate::log_info;
 use crate::terminal::command_manager::CommandManager;
 use crate::utils::logger::Logger;
+
 pub struct CmdMe;
 
 impl CommandManager for CmdMe {
     fn execute(_args: Vec<&str>) -> Result<(), String> {
-        let pid = std::process::id();
+        let pid = Pid::from_u32(std::process::id());
 
-        let mut process = match Process::new(pid) {
-            Ok(process) => process,
-            Err(e) => return Err(e.to_string()),
+        match System::new().process(pid) {
+            Some(process) => print_info(&process),
+            None => return Err("Failed to get the System Info".to_string()),
         };
-
-        let cpu_percent = match process.cpu_percent() {
-            Ok(cpu_percent) => cpu_percent,
-            Err(e) => return Err(e.to_string()),
-        };
-
-        let ram_info = match process.memory_info() {
-            Ok(ram_info) => ram_info,
-            Err(e) => return Err(e.to_string()),
-        };
-
-        log_info!("------------>Cloud Info<------------");
-        log_info!("Cpu: {:.2}%", cpu_percent);
-        log_info!("Ram {} Bytes", ram_info.rss());
-        log_info!("------------------------------------");
 
         Ok(())
     }
@@ -35,4 +21,11 @@ impl CommandManager for CmdMe {
     fn tab_complete(_args: Vec<&str>) -> Vec<String> {
         todo!()
     }
+}
+
+fn print_info(process: &sysinfo::Process) {
+    log_info!("------------>Cloud Info<------------");
+    log_info!("Cpu: {:.2}%", process.cpu_usage());
+    log_info!("Ram {} Bytes", process.memory());
+    log_info!("------------------------------------");
 }
